309784ac45849678cf54cbb45f8442bbebbd7684
kuber <kuberaaganja@gmail.com>
2012-07-27 14:33:49 +0545

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RevolTechComNp::Application.config.secret_token = '22bd7fae9992c16dcdbcbc81bf272a05f69c695b044fcd671d100cbfa21d91b2cb201828b632478362079547b8106cb5ff55eb74dae3feb7ae659ba69127cf67'
